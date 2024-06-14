db76e4020a0c18e85c26fe6fa79879e666f9dc74
Steve Pringle <nyhtml@users.noreply.github.com>
2024-06-14 15:32:26 -0400

    Add files via upload

---
+++ Essex.cmd
@@ -0,0 +1,38 @@
+TITLE GHOST v2024.06 by Stephan Pringle
+@ECHO OFF
+COLOR 4F
+CLS
+ECHO Installing Zoom Client for Meetings
+"Apps\ZoomInstallerFull.exe"
+ECHO Installing JavaScript v6
+"Apps\jre-6u25-windows-i586-s.exe"
+ECHO Installing JavaScript v7
+"Apps\jre-7-windows-x64.exe"
+ECHO Installing Opera Browser
+"Apps\OperaSetup.exe"
+ECHO Installing Firefox
+"Apps\Firefox Installer.exe"
+ECHO Installing Google Chrome
+"Apps\ChromeSetup.exe"
+ECHO Installing Cisco AnyConnect
+"Apps\anyconnect.msi"
+ECHO Installing RSA SurePDF
+"Apps\essex_SurePDF.msi"
+ECHO Installing Acro Software CutePDF
+"Apps\CuteWriter.exe"
+ECHO Installing Microsoft .NET Framework
+"Apps\dotnetfx35.exe"
+ECHO Installing  VideoLAN VLC media player
+"Apps\vlc-3.0.20-win64.exe"
+ECHO Installing Notepad++
+"Apps\npp.exe"
+ECHO Installing Adobe Reader DC
+"Apps\readerdc.exe"
+ECHO Installing Microsoft Office 2019/2021/365
+"MOPPI.cmd"
+COLOR 0A
+CLS
+ECHO The installation is complete.
+ECHO.
+ECHO You may close this window.
+cmd /k
